22cb26809769266f75665049105b9bb176518978
ptyagicodecamp <ptyagicodecamp@gmail.com>
2020-02-07 23:22:18 -0800

    added hello flutter project

--- lib/main_MaterialApp.dart
+++ lib/main_MaterialApp.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+//Demonstrates the MaterialApp's simplicity over WidgetsApp way of creating an app
+
 void main() => runApp(MyWidget());
 
 class MyWidget extends StatelessWidget {
