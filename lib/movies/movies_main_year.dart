e122dc16c1e2b41dfd6ec1df4fdc7beb36aacd82
ptyagicodecamp <ptyagicodecamp@gmail.com>
2020-02-17 14:45:46 -0800

    added code recipe for last three years popular movies

--- lib/movies/movies_main_year.dart
+++ lib/movies/movies_main_year.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-//Model + backdrop blurred
+//Popular movies from last three years
 void main() => runApp(MoviesApp());
 
 class MoviesApp extends StatelessWidget {
