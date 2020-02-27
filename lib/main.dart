ccb55c4bc1cecc74dcdd04e6ed22df7fe4c8ea1b
ptyagicodecamp <ptyagicodecamp@gmail.com>
2020-02-27 11:51:58 -0800

    updated gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,45 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(ContactProfilePage());
+
+class ContactProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: MyWidget(),
-      ),
-    ));
-
-class MyWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        BlueBox(),
-        BlueBox(),
-        BlueBox(),
-      ],
-    );
-  }
-}
-
-class BlueBox extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 50,
-      height: 50,
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        border: Border.all(),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.star_border),
+              color: Colors.black,
+              onPressed: () {
+                print("Contact is starred");
+              },
+            ),
+          ],
+        ),
+        body: ListView(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Container(
+                  width: double.infinity,
+                  height: 250,
+                  child: Image.asset(
+                    "assets/profile.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/movies/bloc_pattern/repository/repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_course/movies/bloc_pattern/model/movie.dart';
-import 'movies_provider.dart';
-
-class Repository {
-  final moviesAPI = MoviesProvider();
-
-  Future<List<Movie>> moviesByTheYear(String year) =>
-      moviesAPI.getBestMoviesByYear(year);
-}
